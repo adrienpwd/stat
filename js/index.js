2f644418b5cddf70ea6ff06d4f1c658aae732c39
Adrien Poirson <adrien.poirson@calabrio.com>
2018-06-22 10:48:06 -0700

    WIP

--- js/index.js
+++ js/index.js
@@ -7,11 +7,28 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import Routes from './routes';
 
-const app = document.getElementById('root');
-const middleware = applyMiddleware(thunk);
-const enhancer = compose(middleware);
+// Polyfill for React use of requestAnimationFrame
+import 'raf/polyfill';
 
-const store = createStore(rootReducer, enhancer);
+const clientMiddleware = [];
+const middleware = [...new Set([thunk, ...clientMiddleware])];
+const enhancers = [];
+const initialState = {};
+
+if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+  enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__());
+}
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    ...enhancers
+  )
+);
+
+const app = document.getElementById('root');
 
 ReactDOM.render(
   <Provider store={store}>
